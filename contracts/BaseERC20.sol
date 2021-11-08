ae10ef192958546f94cfaf9954c37fdf62e375c3
big-javascript-fan <wdeveloping9099@gmail.com>
2021-11-08 09:56:39 -0500

    refatorg

--- contracts/BaseERC20.sol
+++ contracts/BaseERC20.sol
@@ -4,21 +4,19 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BaseERC20 is ERC20, Ownable {    
-    
+contract BaseERC20 is ERC20, Ownable {
     /**
-     * @dev Sets the values for {name} and {symbol}.    
+     * @dev Sets the values for {name} and {symbol}.
      * All two of these values are immutable: they can only be set once during
      * construction.
      */
-    constructor() ERC20("Rewarding", "GLD") {
-    }
+    constructor() ERC20("Rewarding", "GLD") {}
 
     /** @dev Allows the only owner to mint
      * Creates `amount` tokens and assigns them to `account`, increasing
      * the total supply.
      */
-    function mintBaseToken(address account, uint256 amount) public onlyOwner{
+    function mintBaseToken(address account, uint256 amount) public onlyOwner {
         _mint(account, amount);
     }
 }
--- contracts/StakingRewards.sol
+++ contracts/StakingRewards.sol
@@ -5,7 +5,6 @@ import "./BaseERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract StakingRewards {
-
     using SafeMath for uint256;
     BaseERC20 public BaseERC20Token;
 
@@ -13,38 +12,44 @@ contract StakingRewards {
 
     uint256 rewardRate = 1;
     uint256 fixedTotalSupply = 800;
-    struct StakeList{
+    struct StakeList {
         address walletAddr;
-        uint256 stakesAmount; 
+        uint256 stakesAmount;
         uint256 rewardsAmount;
         uint256 lastUpdateTime;
     }
 
     /**
-    * @notice The accumulated stake status for each stakeholder.
-    */
-    mapping(address => StakeList) public stakeLists;    
+     * @notice The accumulated stake status for each stakeholder.
+     */
+    mapping(address => StakeList) public stakeLists;
     address[] public stakeListActs;
-    
+
     /**
-    * @dev Creates a staking contract that handles the staking, unstaking, getReward features
-    * for BaseERC20 tokens.
-    * @param _tokenAddress BaseERC20 contract addresss that is already deployed
-    */    
-    constructor (BaseERC20 _tokenAddress) {
+     * @dev Creates a staking contract that handles the staking, unstaking, getReward features
+     * for BaseERC20 tokens.
+     * @param _tokenAddress BaseERC20 contract addresss that is already deployed
+     */
+    constructor(BaseERC20 _tokenAddress) {
         BaseERC20Token = _tokenAddress;
     }
 
     /**
-    * @notice A method for this contract stakeholder to create a stake.
-    * uint256 The amount of wei in this method.
-    * @param _amount The size of the stake to be created.
-    */
-    function stake(uint256 _amount) public returns(bool) {              
-        require(BaseERC20Token.balanceOf(msg.sender) >= _amount, "Please deposite more in your card!");
-        require(_amount > 0, "The amount to be transferred should be larger than 0");
+     * @notice A method for this contract stakeholder to create a stake.
+     * uint256 The amount of wei in this method.
+     * @param _amount The size of the stake to be created.
+     */
+    function stake(uint256 _amount) public returns (bool) {
+        require(
+            BaseERC20Token.balanceOf(msg.sender) >= _amount,
+            "Please deposite more in your card!"
+        );
+        require(
+            _amount > 0,
+            "The amount to be transferred should be larger than 0"
+        );
 
-        BaseERC20Token.transferFrom(msg.sender,address(this), _amount);
+        BaseERC20Token.transferFrom(msg.sender, address(this), _amount);
         StakeList storage _personStakeSatus = stakeLists[msg.sender];
         _personStakeSatus.rewardsAmount = updateReward(msg.sender);
         _personStakeSatus.stakesAmount += _amount;
@@ -54,16 +59,22 @@ contract StakingRewards {
     }
 
     /**
-    * @notice A method for this contract stakeholder to unstake.
-    * uint256 The amount of wei in this method.
-    * @param _amount The size of the unstake.
-    */
-    function unStake(uint256 _amount) public returns(bool) {
+     * @notice A method for this contract stakeholder to unstake.
+     * uint256 The amount of wei in this method.
+     * @param _amount The size of the unstake.
+     */
+    function unStake(uint256 _amount) public returns (bool) {
         StakeList storage _personStakeSatus = stakeLists[msg.sender];
 
-        require (_personStakeSatus.stakesAmount != 0, "No stake");
-        require(_amount > 0, "The amount to be transferred should be larger than 0");
-        require(_amount <= _personStakeSatus.stakesAmount, "The amount to be transferred should be less than Deposit");
+        require(_personStakeSatus.stakesAmount != 0, "No stake");
+        require(
+            _amount > 0,
+            "The amount to be transferred should be larger than 0"
+        );
+        require(
+            _amount <= _personStakeSatus.stakesAmount,
+            "The amount to be transferred should be less than Deposit"
+        );
 
         BaseERC20Token.transfer(msg.sender, _amount);
         _personStakeSatus.rewardsAmount = updateReward(msg.sender);
@@ -74,13 +85,13 @@ contract StakingRewards {
     }
 
     /**
-    * @notice A method to allow a stakeholder to check his rewards.
-    * uint256 The amount of wei in this method.
-    */
-    function getReward() public returns(bool) {
+     * @notice A method to allow a stakeholder to check his rewards.
+     * uint256 The amount of wei in this method.
+     */
+    function getReward() public returns (bool) {
         StakeList storage _personStakeSatus = stakeLists[msg.sender];
 
-        require (_personStakeSatus.rewardsAmount != 0, "No rewards");
+        require(_personStakeSatus.rewardsAmount != 0, "No rewards");
 
         _personStakeSatus.rewardsAmount = updateReward(msg.sender);
         uint256 getRewardAmount = _personStakeSatus.rewardsAmount;
@@ -88,62 +99,65 @@ contract StakingRewards {
         _personStakeSatus.rewardsAmount = 0;
         updateStakeList(msg.sender);
 
-        emit RewardPaid(msg.sender, getRewardAmount); 
+        emit RewardPaid(msg.sender, getRewardAmount);
 
         return true;
     }
 
     /**
-    * @notice A method to calcaulate the stake rewards for a stakeholder for all transactions.
-    * @param _account The stakeholder to retrieve the stake rewards for.
-    * @return uint256 The amount of ethers.
-    */
+     * @notice A method to calcaulate the stake rewards for a stakeholder for all transactions.
+     * @param _account The stakeholder to retrieve the stake rewards for.
+     * @return uint256 The amount of ethers.
+     */
     function updateReward(address _account) public view returns (uint256) {
         StakeList storage _personStakeSatus = stakeLists[_account];
 
         if (_personStakeSatus.stakesAmount == 0) {
             return _personStakeSatus.rewardsAmount;
         }
-        return _personStakeSatus.rewardsAmount
-            .add(block.timestamp
-            .sub(_personStakeSatus.lastUpdateTime)
-            .mul(_personStakeSatus.stakesAmount)
-            .mul(rewardRate)
-            .div(fixedTotalSupply)
-            .div(10000));   // it means that rewardRate is 0.001%
+        return
+            _personStakeSatus.rewardsAmount.add(
+                block
+                    .timestamp
+                    .sub(_personStakeSatus.lastUpdateTime)
+                    .mul(_personStakeSatus.stakesAmount)
+                    .mul(rewardRate)
+                    .div(fixedTotalSupply)
+                    .div(10000)
+            ); // it means that rewardRate is 0.001%
     }
 
     /**
-    * @notice A method to update or push stake status for all transactions.
-    * If an account is new, push his stake status while an already existing user, update them.
-    * @param account The stakeholder to retrieve the stake status.
-    */
+     * @notice A method to update or push stake status for all transactions.
+     * If an account is new, push his stake status while an already existing user, update them.
+     * @param account The stakeholder to retrieve the stake status.
+     */
     function updateStakeList(address account) public {
         StakeList storage personStakeSatus = stakeLists[account];
 
-        if (personStakeSatus.lastUpdateTime == 0) {       
+        if (personStakeSatus.lastUpdateTime == 0) {
             stakeListActs.push(account);
         }
         personStakeSatus.lastUpdateTime = block.timestamp;
     }
 
     /**
-    * @notice A method to retrieve the stake for a stakeholder.
-    * @param _stakeholder The stakeholder to retrieve the stake for.
-    * @return uint256 The amount of ethers.
-    */
-    function stakeOf(address _stakeholder) public view returns(uint256) {
-        StakeList storage _personStakeSatus = stakeLists[_stakeholder];        
+     * @notice A method to retrieve the stake for a stakeholder.
+     * @param _stakeholder The stakeholder to retrieve the stake for.
+     * @return uint256 The amount of ethers.
+     */
+    function stakeOf(address _stakeholder) public view returns (uint256) {
+        StakeList storage _personStakeSatus = stakeLists[_stakeholder];
         return _personStakeSatus.stakesAmount;
     }
 
     /**
-    * @notice A method to retrieve the rewards for a stakeholder.
-    * @param _stakeholder The stakeholder to retrieve the rewards for.
-    * @return uint256 The amount of ethers.
-    */
-    function rewardOf(address _stakeholder) public view returns(uint256) {
-        StakeList storage _personStakeSatus = stakeLists[_stakeholder];        
+     * @notice A method to retrieve the rewards for a stakeholder.
+     * @param _stakeholder The stakeholder to retrieve the rewards for.
+     * @return uint256 The amount of ethers.
+     */
+    function rewardOf(address _stakeholder) public view returns (uint256) {
+        StakeList storage _personStakeSatus = stakeLists[_stakeholder];
         return _personStakeSatus.rewardsAmount;
     }
 }
